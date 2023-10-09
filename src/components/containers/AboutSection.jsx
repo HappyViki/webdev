3f540aed1b72a0e0e3cf161e761279773a9e6154
Veronica Eulenberg <veronica.eulenberg@gmail.com>
2023-10-09 14:12:46 -0500

    update text

--- src/components/containers/AboutSection.jsx
+++ src/components/containers/AboutSection.jsx
@@ -55,7 +55,7 @@ const AboutSection = () => {
             Who is <span className="text-primary">{data.fullName}</span>?
           </h3>
           <h4>Senior Front End Web Developer</h4>
-          <p>I have 6 years experience in software development. My strengths are in JavaScript, CSS, React, Node, Mobile Development, and Web Development. Seeking to work with professional people that have a positive attitude and a good sense of humor.</p>
+          <p>I have 6 years of experience in software development. My strengths are in JavaScript, CSS, React, Node, Mobile, and Web Development. Seeking to work with professionals that have a positive attitude and a good sense of humor.</p>
           <p>Located in Dallas, TX.</p>
           <a href="/VeronicaEulenbergResume.pdf" className="btn mt-3">
             <span>Download Resume</span>
